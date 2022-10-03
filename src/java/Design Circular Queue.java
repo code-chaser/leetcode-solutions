6f3afb8cfd85a5682f1458b4d2a5948f4472fe31
Sakshi Mishra <102748498+sakshimishra19@users.noreply.github.com>
2022-10-03 17:35:32 +0530

    Create  Design Circular Queue.java

---
+++ src/java/Design Circular Queue.java
@@ -0,0 +1,78 @@
+/*Question:-Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle, and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implement the MyCircularQueue class:
+
+MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+int Front() Gets the front item from the queue. If the queue is empty, return -1.
+int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+boolean isEmpty() Checks whether the circular queue is empty or not.
+boolean isFull() Checks whether the circular queue is full or not.*/
+
+class MyCircularQueue {
+
+    private int front;
+    private int rear;
+    private int[] arr;
+    private int cap;
+    
+    private int next(int i){ // to get next idx after i in circular queue
+        return (i+1)%cap;
+    }
+    private int prev(int i){ // to get prev idx before i in circular queue
+        return (i+cap-1)%cap;
+    }
+    
+	// rest is as simple as implmenting a normal queue using array.
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        cap=k;
+        front=-1;
+        rear=-1;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull())return false;
+        if(front==-1){
+            front=0;
+            rear=0;
+            arr[rear]=value;
+            return true;
+        }
+        rear = next(rear);
+        arr[rear]=value;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty())return false;
+        if(front==rear){
+            front=-1;
+            rear=-1;
+            return true;
+        }
+        front=next(front);
+        return true;
+    }
+    
+    public int Front() {
+        if(front==-1)return -1;
+        return arr[front];
+    }
+    
+    public int Rear() {
+        if(rear==-1)return -1;
+        return arr[rear];
+    }
+    
+    public boolean isEmpty() {
+        return front==-1;
+    }
+    
+    public boolean isFull() {
+        return front!=-1 && next(rear)==front;
+    }
+}
