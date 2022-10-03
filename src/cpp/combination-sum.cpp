55578c6f633ea74de7957d2057215ed86acfcd62
Sneha Pandey <55934654+snehapandey2000@users.noreply.github.com>
2022-10-03 13:47:32 +0530

    Create combination-sum.cpp

---
+++ src/cpp/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target, vector<vector<int>> &ans,vector<int> &combination, int start){
+        if(target==0){
+            ans.push_back(vector<int> (combination.begin(), combination.end()));
+            return;
+        }
+        for(int i=start;i<candidates.size();i++){
+            if(candidates[i]>target){
+                break;
+            }
+            combination.push_back(candidates[i]);
+            helper(candidates, target-candidates[i],ans,combination,i);
+            combination.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<vector<int>> ans;
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates,target,ans,combination,0);
+        return ans;
+    }
+};
