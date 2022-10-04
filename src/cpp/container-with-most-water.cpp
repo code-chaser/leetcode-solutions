f880329c16adff8c1b3a49d22dec361038bb8670
PeritusDeveloper <89828000+PeritusDeveloper@users.noreply.github.com>
2022-10-04 18:45:42 +0530

    Update container-with-most-water.cpp

--- src/cpp/container-with-most-water.cpp
+++ src/cpp/container-with-most-water.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int maxArea(vector<int>& h) {
-        // int are=0;
-        // for(int i=0;i<height.size();i++){
-        //     for(int j=i+1;j<height.size();j++){
-        //         int mini=min(height[i],height[j]);
-        //         int bre=j-i;
-        //         are=max(are,mini*bre);
-        //     }
-        // }
-        // return are;
         int start=0;
         int end=h.size()-1;
         int max_area=0;
