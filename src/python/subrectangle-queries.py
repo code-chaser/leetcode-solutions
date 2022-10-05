d406299277df237e662e13335e56131f7c124c56
ThatOneKrish <csrealracing13d@gmail.com>
2022-10-05 10:24:55 +0530

    Create subrectangle-queries.py

---
+++ src/python/subrectangle-queries.py
@@ -0,0 +1,10 @@
+ def __init__(self, rectangle: List[List[int]]):
+        self.r = rectangle        
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for i in range(row1, row2 + 1):
+            for j in range(col1, col2 + 1):
+                self.r[i][j] = newValue
+
+    def getValue(self, row: int, col: int) -> int:
+        return self.r[row][col]
