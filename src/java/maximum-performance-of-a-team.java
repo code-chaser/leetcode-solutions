b5abd3a7287670ba7cdc05c06f31199b27fd6f27
Sakshi Mishra <102748498+sakshimishra19@users.noreply.github.com>
2022-10-09 21:31:23 +0530

    Create maximum-performance-of-a-team.java

---
+++ src/java/maximum-performance-of-a-team.java
@@ -0,0 +1,42 @@
+/*Question:-You are given two integers n and k and two integer arrays speed and efficiency both of length n. There are n engineers numbered from 1 to n. speed[i] and efficiency[i] represent the speed and efficiency of the ith engineer respectively.
+
+Choose at most k different engineers out of the n engineers to form a team with the maximum performance.
+
+The performance of a team is the sum of their engineers' speeds multiplied by the minimum efficiency among their engineers.
+
+Return the maximum performance of this team. Since the answer can be a huge number, return it modulo 109 + 7.
+*/
+
+//Solution
+
+
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] players = new int[n][2];
+        for (int i=0; i<n; i++) {
+            players[i][0] = efficiency[i];
+            players[i][1] = speed[i];
+        }
+		// Sort the players based on efficiency in decreasing order, as for each iteration, we'll consider only players with higher efficiency.
+        Arrays.sort(players, (p1, p2) -> (p2[0] - p1[0]));
+
+		// Priority-Queue to maintain players with highest relative speeds with efficiencies greater than the one under iteration.
+        PriorityQueue<Integer> speedQueue = new PriorityQueue<>(k);
+        long teamPerformance = 0, teamSpeed = 0;
+
+        for (int i=0; i<n; i++) {
+			// This is because a team can have atmost `k` players.
+            if (speedQueue.size() >= k) {
+                teamSpeed -= speedQueue.remove();
+            }
+            speedQueue.add(players[i][1]);
+            teamSpeed += players[i][1];
+
+            teamPerformance = Math.max(teamPerformance, teamSpeed * players[i][0]);
+        }
+        return (int) (teamPerformance % 1000000007);
+    }
+}
+
+
+//Submission link:-https://leetcode.com/submissions/detail/797195537/
